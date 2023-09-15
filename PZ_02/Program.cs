6ed840349ce2c43226a437a75b15e92ec877b119
Андрей Дубровский <nataladubrovskaa38724@gmail.com>
2023-09-15 13:01:13 +0300

    Добавлена практика 2

---
+++ PZ_02/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace PZ_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число j");
+            double  j = double.Parse(Console.ReadLine());
+
+            double x;
+            
+            double y;
+
+            double z;
+            Console.WriteLine("Введите число i");
+            int i = int.Parse(Console.ReadLine());
+
+            if (j > 0)
+                x = i * j + Math.Sin(j);
+            else x = (i + j) / (Math.Sqrt(2 * j + i));
+            if (x > 0)
+                y = 15 * (Math.Sqrt(x - 0.5 * j)) + x;
+            else y = Math.Cos(i + x);
+
+            z = (x + y) / (Math.Pow(i, 2) + Math.Pow(j, 2) + 1);
+
+
+            Console.WriteLine("i = " + i);
+            Console.WriteLine("j = " + j);
+            Console.WriteLine("x = " + Math.Round(x, 2));
+            Console.WriteLine("y = " + Math.Round(y, 2));
+            Console.WriteLine("z = " + Math.Round(z, 2));
+
+
+
+        }
+    }
+}
