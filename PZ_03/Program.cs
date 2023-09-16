da750ee8e97f041347597d4b7a17b4fbaae6dc7e
Андрей Дубровский <nataladubrovskaa38724@gmail.com>
2023-09-16 08:43:14 +0300

    Добавлена 3 практика

---
+++ PZ_03/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace PZ_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            int x = int.Parse(Console.ReadLine());
+            int caseSwitch = x;
+
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.WriteLine(" Декабрь - 31 день, Январь - 31 день, Февраль - 28 дней");
+                    break;
+                case 2:
+                    Console.WriteLine(" Март -31 день, Апрель - 30 дней, Май - 31 день ");
+                    break;
+                case 3:
+                    Console.WriteLine(" Июнь - 30 дней, Июль 31 день, Август - 31 день ");
+                    break;
+                case 4:
+                    Console.WriteLine(" Сентябрь - 30 дней, Октябрь - 31 день, Ноябрь - 30 дней ");
+                    break;
+
+
+
+
+
+            }
+
+        }
+    }
+}
