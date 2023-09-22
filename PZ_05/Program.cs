90b5d9605bba8eedacafea62771446bb97069263
Pooh <Pooh@DESKTOP-V8KM2M2>
2023-09-22 14:17:30 +0500

    Добавлена 5 практика

---
+++ PZ_05/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Целые степени двойки, не превосходящие N:");
+
+            int a = 1;
+
+            while (a <= N)
+            {
+                Console.WriteLine(a);
+                a = a << 1; // умножение на 2 через сдвиг
+            }
+        }
+        
+    }
+}
