72680b1faa0d8663ba896295507f8fd44d2cf409
Андрей Дубровский <nataladubrovskaa38724@gmail.com>
2023-09-30 08:27:56 +0300

    Добавлена практика 7

---
+++ PZ_07/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace PZ_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[7, 7];
+            Random random = new Random();
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    array[i, j] = random.Next(-10, 11);
+                }
+               
+            }
+            Console.WriteLine("Главная диагональ массива:");
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write(array[i, i] + " ");
+            }
+            Console.WriteLine();
+
+            // Подсчитываем количество положительных элементов на главной диагонали
+            int l = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                if (array[i, i] > 0)
+                {
+                    l++;
+                }
+            }
+            Console.WriteLine($"Количество положительных элементов на главной диагонали: {l}");
+        }
+
+
+    }
+    
+}
