2d6534ac701da0b02d4fba0127c0d120d86472ba
Pooh <Pooh@DESKTOP-V8KM2M2>
2023-12-01 18:55:36 +0500

    Добавлена практика 12

---
+++ PZ_12/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PZ_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static int CountSubstringOccurrences(string str, string subStr)
+            {
+                int count = 0;
+                int lastIndex = 0;
+
+                while ((lastIndex = str.IndexOf(subStr, lastIndex)) != -1)
+                {
+                    count++;
+                    lastIndex += subStr.Length;
+                }
+
+                return count;
+            }
+            string str = Console.ReadLine();
+            string subStr = Console.ReadLine();
+            int Bxod = CountSubstringOccurrences(str, subStr);
+            Console.WriteLine($"Количество вхождений '{subStr}' в строку '{str}': {Bxod}");
+        }
+    }
+}
