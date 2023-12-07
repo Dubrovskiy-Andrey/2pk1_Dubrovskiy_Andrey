fd6938f3a43f36dca4fe45b55ab1b8c4d6325e62
dubrovskiy.as2179 <dubrovskiy.as2179@COM402-R3>
2023-12-07 12:12:28 +0300

    Добавлена практика 14

---
+++ PZ_14/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.IO;
+namespace PZ_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                {
+                    string file = "file.txt";
+
+                    int k = 5;
+                    int l = 8;
+
+                    int[,] array = new int[k, l];
+
+                    // Заполнение массива и расчет элементов по правилу A[i, j] = i * j
+                    for (int i = 0; i < k; i++)
+                    {
+                        for (int j = 0; j < l; j++)
+                        {
+                            array[i, j] = i * j;
+                        }
+                    }
+
+                    // Запись массива в файл с использованием потока записи
+                    using (StreamWriter writer = new StreamWriter(file))
+                    {
+                        for (int i = 0; i < k; i++)
+                        {
+                            for (int j = 0; j < l; j++)
+                            {
+                                writer.Write($"{array[i, j]} ");
+                            }
+
+                            writer.WriteLine();
+                        }
+                    }
+
+                    Console.WriteLine("Запись в файл успешно выполнена.")ж
+                }
+            
+
+        }
+    }
+}
+    
